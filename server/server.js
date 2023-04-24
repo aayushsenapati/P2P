c3d04f8e119e42cb79d4de1b7621d497120dff13
Aadithya H Rao <aadithyarao@gmail.com>
2023-04-24 10:34:36 +0530

    made origin *

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ const http = require('http').createServer(app);
 const { ExpressPeerServer } = require('peer');
 const io = require('socket.io')(http, {
     cors: {
-        origin: "http://localhost:3000",
+        origin: "*",
         methods: ["GET", "POST"]
     }
 });
